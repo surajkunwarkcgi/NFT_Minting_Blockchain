19bd831c08819e9b55e0834e73f5e5073773779e
surajkunwarkcgi <kunwarsuraj2019@gmail.com>
2022-12-08 20:28:59 +0900

    Alert and loading file created; smart contract created

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Artworks from "./components/Artworks";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Alert from "./components/Alert";
+import Loading from "./components/Loading";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       </div>
       <Artworks />
       <Footer />
+      <Alert />
+      <Loading />
     </div>
   );
 };
